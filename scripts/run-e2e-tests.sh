5e92bf72bc34339c0ad1d225cf4332cf247ebcaf
Annamaria Szegedi <aszegedi@hortonworks.com>
2016-11-09 11:02:48 +0100

    Introduce WebDriver Manager Update to the Entry script

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -5,9 +5,12 @@ cd $HOME
 # Remove previous Allure results
 rm -rf allure-results
 
-#Install the necessary npm packages
+# Install the necessary npm packages
 npm install
-
+# Run the Selenium installation script, located in the local node_modules/ directory.
+# This script downloads the files required to run Selenium itself and build a start script and a directory with them.
+# When this script is finished, we can start the standalone version of Selenium with the Chrome driver by executing the start script.
+node ./node_modules/protractor/bin/webdriver-manager update
 # X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
 # Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
 Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
@@ -16,7 +19,7 @@ Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 
 # Right now this is not necessary, because of 'directConnect: true' in the 'e2e.conf.js'
 #echo "Starting webdriver"
-#webdriver-manager start &
+#node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
 #echo "Finished starting webdriver"
 #sleep 20
 
