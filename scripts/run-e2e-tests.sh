0715f819e755d0cb828b36c6179302f428e4845d
Annamaria Szegedi <aszegedi@hortonworks.com>
2016-12-12 14:59:44 +0100

    Set Protractor troubleshoot for test runs

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -25,7 +25,7 @@ Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 
 echo "Running Protractor tests"
 # The 'uluwatu-e2e-protractor' test project launch configuration file (e2e.conf.js) should be passed here.
-DISPLAY=:10 protractor $@
+DISPLAY=:10 protractor --troubleshoot $@
 export RESULT=$?
 
 echo "Protractor tests have done"
