51edaa36d903dd194f7d80c16fc9532397772a6a
Stanisław Nowak <stanislaw.nowak@thaumatec.com>
2018-05-22 13:12:24 +0200

    Dont call protractor directly

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -14,6 +14,7 @@ node ./node_modules/protractor/bin/webdriver-manager update
 # X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
 # Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-1280x1024x24}
+export DISPLAY=:10
 echo "Screen resolution ${SCREEN_RESOLUTION}"
 Xvfb :10 -screen 0 $SCREEN_RESOLUTION 2>&1 >/dev/null &
 XVFB_PID=$!
@@ -28,7 +29,7 @@ sleep 20
 
 echo "Running Protractor tests"
 # The 'uluwatu-e2e-protractor' test project launch configuration file (e2e.conf.js) should be passed here.
-DISPLAY=:10 protractor $@
+exec $@
 export RESULT=$?
 
 echo "Protractor tests have done"
